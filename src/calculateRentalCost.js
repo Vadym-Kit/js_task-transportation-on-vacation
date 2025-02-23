aa89563509bd9d64fb47c2529287a018bf1a6f46
Vadym-Kit <superovyq@gmail.com>
2025-02-23 23:36:21 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const COUNT__CASH = 40 * days;
+  const LONG__TEMP = 7;
+  const SHORT__TEMP = 3;
+  const LONG__TEMP__DISCOUNT = 50;
+  const SHORT__TEMP__DISCOUNT = 20;
+
+  if (days >= LONG__TEMP) {
+    return COUNT__CASH - LONG__TEMP__DISCOUNT;
+  }
+
+  if (days >= SHORT__TEMP) {
+    return COUNT__CASH - SHORT__TEMP__DISCOUNT;
+  }
+
+  return COUNT__CASH;
 }
 
 module.exports = calculateRentalCost;
